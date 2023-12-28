29e046fb8a9140aa8ceadb1338d3d447bf688e20
Francesco <francesco0501@libero.it>
2023-12-28 22:31:16 +0100

    cleaned config.py and fixed alembic upgrade head bug on docker container

--- app/config.py
+++ app/config.py
@@ -1,19 +1,5 @@
-# from pydantic_settings import BaseSettings
-# import os
-
-# class Settings(BaseSettings):
-#     database_hostname: str=os.getenv("DATABASE_HOSTNAME")
-#     database_port: str=os.getenv("DATABASE_PORT")
-#     database_password: str=os.getenv("DATABASE_PASSWORD")
-#     database_name: str=os.getenv("DATABASE_NAME")
-#     database_username: str=os.getenv("DATABASE_USERNAME")
-#     secret_key: str=os.getenv("SECRET_KEY")
-#     algorithm: str=os.getenv("ALGORITHM")
-#     access_token_expire_minutes: int=os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
-    
-# settings = Settings()
-
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
 class Settings(BaseSettings):
     database_hostname: str
     database_port: str
@@ -24,6 +10,6 @@ class Settings(BaseSettings):
     algorithm: str
     access_token_expire_minutes: int
 
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file=".env") # not needed if using docker
 
 settings = Settings()
